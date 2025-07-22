10a34a790cab10c4bb6f1e2bcc54fffae0c1e42b
tml <1581285032@qq.com>
2025-07-22 16:40:07 +0800

    init

--- modules/__init__.py
+++ modules/__init__.py
@@ -3,4 +3,3 @@ from .persona_manager import PersonaManager
 from .personalized_agent import PersonalizedChatbot
 from .user_simulator import User
 from .tool_simulator import APICaller
-from .evaluator import FineGrainEvaluator
